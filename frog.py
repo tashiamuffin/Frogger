43e0b5366de94ed50cbf08cabe0c5fc450e76909
Natalia Lach <262303@student.pwr.edu.pl>
2021-05-23 16:39:55 +0200

    interakcje zabki z obiektami

--- frog.py
+++ frog.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 from sys import exit
 import random
 
+#### TRZEBA TERAZ DODAĆ WPADANIE DO WODY + LICZNIK CZASU + LICZNIK ŻYĆ  
 SCREEN_WIDTH = 1102
 SCREEN_HEIGHT = 804
 pygame.init()
@@ -35,23 +36,34 @@ class Frog(pygame.sprite.Sprite):
         self.rect = self.image.get_rect() #rozmiar rysunku
         self.position = [SCREEN_WIDTH/2, SCREEN_HEIGHT - 25]
         self.rect.center = (self.position[0], self.position[1]) #pozycja początkowa
-        self.x_velocity = 0
-        self.y_velocity = 0
+        self.ability = 1 ###umiejętność poruszania się, gdy jest -1 to nie może się ruszać ( jest już na górze szczęsliwa i czeka na inne żabki)
 
     def update(self):
-        self.rect.center = (self.position[0],self.position[1])#move in-place
+        if self.ability == 1: ##jeśli może się poruszać to
+            self.rect.center = (self.position[0],self.position[1])#move in-place
 
-        if self.rect.left < 0:
-            self.rect.left = 0
-        elif self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
+            if self.rect.left < 0:
+                self.rect.left = 0
+            elif self.rect.right > SCREEN_WIDTH:
+                self.rect.right = SCREEN_WIDTH
 
-        if self.rect.top <= SCREEN_HEIGHT*1/10: #bez dwóch pasów u góry
-            self.rect.top = SCREEN_HEIGHT*1/10
-        elif self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
+            if self.rect.bottom >= SCREEN_HEIGHT:
+                self.rect.bottom = SCREEN_HEIGHT
 
-###auta
+        elif self.ability == 0: ## jej ostatni skok na bezpieczne miejsce
+            self.rect.center = (self.position[0],self.position[1])#move in-place
+
+            if self.rect.left < 0:
+                self.rect.left = 0
+            elif self.rect.right > SCREEN_WIDTH:
+                self.rect.right = SCREEN_WIDTH
+
+            if self.rect.bottom >= SCREEN_HEIGHT:
+                self.rect.bottom = SCREEN_HEIGHT
+
+            self.ability -=1
+
+### ----------------auta---------------
 class cars(pygame.sprite.Sprite):
   
     def __init__(self):
@@ -65,13 +77,14 @@ class cars(pygame.sprite.Sprite):
 
     def update(self):
         self.position[0] += self.x_velocity
-        self.rect.center = (self.position[0],self.position[1])#move in-place
+        self.rect.center = (self.position[0],self.position[1])
         
-        if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
+        if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH: ##gdy wyjdzie poza ekran to znika
             self.kill()
 
+### ------------- kłody -----------
 
-class logsl(pygame.sprite.Sprite):
+class logsl(pygame.sprite.Sprite): ###kłody płynące w lewo
   
     def __init__(self):
         #360, 300, 240, 180, - pasy
@@ -89,7 +102,7 @@ class logsl(pygame.sprite.Sprite):
         if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
             self.kill()
 
-class logsr(pygame.sprite.Sprite):
+class logsr(pygame.sprite.Sprite): ###kłody płynące w prawo
   
     def __init__(self):
         #360, 300, 240, 180 - pasy
@@ -107,16 +120,16 @@ class logsr(pygame.sprite.Sprite):
         if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
             self.kill()
 
+## ----------------main -------------
+
 frog_sprite = pygame.sprite.RenderClear() #kontener na żabe
 frog = Frog()                       #stwórz żabe
 frog_sprite.add(frog) 
 
 # Inicjalizuj statki wroga
-cars_sprites = pygame.sprite.RenderClear() #kontener dla statków
+cars_sprites = pygame.sprite.RenderClear() #kontener dla aut
 cars_sprites.add(cars())
-
-
-logs_sprites = pygame.sprite.RenderClear()
+logs_sprites = pygame.sprite.RenderClear() # kontener dla logs
 
 
 clock = pygame.time.Clock()
@@ -125,6 +138,8 @@ sit = True
 addenemyfighterCounter = 0
 addlogsl = 0
 addlogsr = 0
+lives = 3
+
 while sit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -139,11 +154,50 @@ while sit:
                 frog.position[0] = frog.position[0] + 60
                 frog.image = pygame.image.load(r'C:\Users\admin\OneDrive\Pulpit\informatyka\gra\zaba_right.png').convert_alpha()
             elif event.key == K_UP:
-                frog.position[1] = frog.position[1] - 60
-                frog.image = pygame.image.load(r'C:\Users\admin\OneDrive\Pulpit\informatyka\gra\zaba.png').convert_alpha()
+                ####gdy żaba jest u samej góry i chce wejść na bezpieczną lilię
+                if frog.position[1] < 130 and 520<frog.position[0]<570:
+                    frog.position[1] = 60
+                    frog.position[0] = 545
+                    frog.ability = 0
+                    frog.image = pygame.image.load(r'C:\Users\admin\OneDrive\Pulpit\informatyka\gra\zaba_down.png').convert_alpha()
+                    frog = Frog()
+                    frog_sprite.add(frog) 
+                elif frog.position[1] < 130 and 150<frog.position[0]<220:
+                    frog.position[1] = 60
+                    frog.position[0] = 185
+                    frog.ability = 0
+                    frog.image = pygame.image.load(r'C:\Users\admin\OneDrive\Pulpit\informatyka\gra\zaba_down.png').convert_alpha()
+                    frog = Frog()
+                    frog_sprite.add(frog) 
+                elif frog.position[1] < 130 and 335<frog.position[0]<400:
+                    frog.position[1] = 60
+                    frog.position[0] = 365
+                    frog.ability = 0
+                    frog.image = pygame.image.load(r'C:\Users\admin\OneDrive\Pulpit\informatyka\gra\zaba_down.png').convert_alpha()
+                    frog = Frog()
+                    frog_sprite.add(frog) 
+                elif frog.position[1] < 130 and 690<frog.position[0]<760:
+                    frog.position[1] = 60
+                    frog.position[0] = 725
+                    frog.ability = 0
+                    frog.image = pygame.image.load(r'C:\Users\admin\OneDrive\Pulpit\informatyka\gra\zaba_down.png').convert_alpha()
+                    frog = Frog()
+                    frog_sprite.add(frog) 
+                elif frog.position[1] < 130 and 880<frog.position[0]<930:
+                    frog.position[1] = 60
+                    frog.position[0] = 905
+                    frog.ability = 0
+                    frog.image = pygame.image.load(r'C:\Users\admin\OneDrive\Pulpit\informatyka\gra\zaba_down.png').convert_alpha()
+                    frog = Frog()
+                    frog_sprite.add(frog) 
+                ### zwyczajny ruch w górę
+                elif frog.position[1] > 130:
+                    frog.position[1] = frog.position[1] - 60
+                    frog.image = pygame.image.load(r'C:\Users\admin\OneDrive\Pulpit\informatyka\gra\zaba.png').convert_alpha()
             elif event.key == K_DOWN:
                 frog.position[1] = frog.position[1] + 60
                 frog.image = pygame.image.load(r'C:\Users\admin\OneDrive\Pulpit\informatyka\gra\zaba_down.png').convert_alpha()
+
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
@@ -166,6 +220,7 @@ while sit:
     addenemyfighterCounter += 1
     addlogsl += 1
     addlogsr += 1
+    ###render aut i logów
 
     if addenemyfighterCounter >= 200:
         cars_sprites.add(cars())
@@ -179,20 +234,40 @@ while sit:
         addlogsr = 0
 
     #Aktualizuj wszystkie sprite'y
-    frog_sprite.update()
+    
     cars_sprites.update()
     logs_sprites.update()
+    frog_sprite.update()
+     
+    ###uderzenie autem
+    for hit in pygame.sprite.groupcollide(cars_sprites,frog_sprite,False, True):
+        frog = Frog()
+        lives -= 1
+        frog_sprite.add(frog)
+        if lives<=0:
+            print("Koniec gry")
+            #sit = False
+
+    ###wejście na kłodę
+    for log_travel in pygame.sprite.groupcollide(logs_sprites, frog_sprite, 0, 0):
+        frog.position[0] += log_travel.x_velocity
+        frog_sprite.update()
+        frog_sprite.draw(screen)
+
 
     #Wyczyść ekran
-    frog_sprite.clear(screen, background)
+    
     cars_sprites.clear(screen, background)
     logs_sprites.clear(screen, background)
+    frog_sprite.clear(screen, background)
 
     #Rysuj sprite'y na ekranie
-    frog_sprite.draw(screen)
+    
     cars_sprites.draw(screen)
     logs_sprites.draw(screen)
+    frog_sprite.draw(screen)
 
     pygame.display.flip()
     
 pygame.quit()
+
